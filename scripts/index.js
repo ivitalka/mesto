ef43023f0846c96a73b5ae98281e9934c61ae9e9
IVitalka <ivitalka24@gmail.com>
2020-10-05 17:16:57 +0700

    bugfix: 'refactor JS'

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@
 let popup = document.querySelector('.popup');
 
 let editButton = document.querySelector('.button_action_edit');
+let closeButton = document.querySelector('.button_action_close');
 
 let formElement = document.querySelector('.popup__container');
 let nameInput = formElement.querySelector('.popup__input_target_name');
@@ -13,27 +14,23 @@ let description = document.querySelector('.profile__description');
 
 
 function popupOpened(){
+    nameInput.value = name.innerText;
+    descriptionInput.value = description.innerText;
     popup.classList.add('popup_opened');
-    nameInput.setAttribute('value', name.innerText);
-    descriptionInput.setAttribute('value', description.innerText);
 }
-
-editButton.addEventListener('click', popupOpened);
-
-let closeButton = document.querySelector('.button_action_close');
 
 function popupClosed() {
     popup.classList.remove('popup_opened');
 }
 
-closeButton.addEventListener('click', popupClosed);
-
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    name.innerText = nameInput.value;
-    description.innerText = descriptionInput.value;
+    name.textContent = nameInput.value;
+    description.textContent = descriptionInput.value;
     popupClosed();
 }
+
+editButton.addEventListener('click', popupOpened);
+closeButton.addEventListener('click', popupClosed);
 
 formElement.addEventListener('submit', formSubmitHandler);
